f85b04ff1a45d832c1acc65f9090d1864876fd93
mathi04 <meyermatthias04@gmail.com>
2026-01-07 13:50:27 +0000

    Revert "dumb db"
    This reverts commit f87b968037a2d6476410f27751440daf6b9fdfbe.

--- web/app/config.py
+++ web/app/config.py
@@ -4,6 +4,6 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret") #cookie key
     SQLALCHEMY_DATABASE_URI = os.environ.get( 
         "DATABASE_URI",
-        "mysql+pymysql://admin_db:unify_admin@db:3306/unify_db"
+        "mysql+pymysql://app_user:app_password@db:3306/app_db"
     ) #connection to db information
     SQLALCHEMY_TRACK_MODIFICATIONS = False
